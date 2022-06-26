253f6501c9df815fc62916da35c5adc10cc5dfc6
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-06-26 17:38:12 -0300

    Rota de post para adm produtos

--- controllers/admController.js
+++ controllers/admController.js
@@ -1,9 +1,38 @@
+const Produto = require('../Models/produto')
 const admController = {
     index: (req, res) => {
         res.render('adm')
     },
     cadastroProduto: (req, res) => {
         res.render('adm/produtos/cadastro')
+    },
+    postPorduto: (req, res) => {
+        const {
+            nome,
+            imagem,
+            tamnhoP,
+            tamanhoM,
+            tamanhoG,
+            preco,
+            estoque,
+            descricao,
+
+        } = req.body
+
+        const produto = {
+            nome,
+            imagem: 'img/produtoExibicao/' + req.file.filename,
+            tamanhoP: (tamnhoP ? 'P' : false),
+            tamanhoM: (tamanhoM ? 'M' : false),
+            tamanhoG: (tamanhoG ? 'G' : false),
+            preco,
+            estoque: (estoque ? 'Em estoque' : 'Sem estoque'),
+            descricao
+        }
+        Produto.save(produto)
+        console.log(produto)
+
+        return res.redirect('/adm')
     }
 }
 
--- routes/adm.js
+++ routes/adm.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const admController = require('../controllers/admController')
+const multerStorage = require('../middlewares/multerStorage')
+
+const uploadIMG = multerStorage('imagem', '/produtoExibicao')
 
 router.get('/adm', admController.index)
 router.get('/adm/produtos/cadastro', admController.cadastroProduto)
+router.post('/adm/produtos/cadastro', uploadIMG, admController.postPorduto)
 
 module.exports = router
