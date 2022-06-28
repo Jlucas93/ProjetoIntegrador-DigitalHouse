a0dd8544ae48cc306c541dadb5149d1b9954e47c
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-06-27 21:28:18 -0300

    admLogin

--- routes/adm.js
+++ routes/adm.js
@@ -8,14 +8,15 @@ const verificarLoginAdm = require('../middlewares/verificarLoginAdm')
 
 const uploadIMG = multerStorage('imagem', '/produtoExibicao')
 
+router.get('/adm/login', admController.login)
+router.get('/logout', admController.logout)
+router.post('/adm/login', admController.postLogin)
 
-router.get('/adm', verificarLoginAdm, admController.index)
+router.use(verificarLoginAdm)
+router.get('/adm', admController.index)
 router.get('/adm/produto/cadastro', admController.cadastroProduto)
 router.post('/adm/produto/cadastro', uploadIMG, admController.postPorduto)
 router.get('/adm/usuario/registro', admController.createUser)
 router.post('/adm/usuario/registro', validarCadastro, admController.postUser)
-router.get('/adm/login', admController.login)
-router.post('/adm/login', admController.postLogin)
-router.get('/logout', admController.logout)
 
 module.exports = router
