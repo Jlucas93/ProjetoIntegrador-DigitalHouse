8859720afb13d22f929238c667185ce671c9c057
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-07-02 19:11:10 -0300

    update

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,26 +2,52 @@ const Produto = require("../Models/produto")
 const fs = require("fs")
 
 const homeController = {
-    index: (req, res, next) => {
+    index: (req, res) => {
         const produtos = Produto.findAll()
         res.render('home/index', { produtos })
     },
-    cadastro: (req, res, next) => {
+    cadastro: (req, res) => {
         res.render('home/cadastro')
     },
-    login: (req, res, next) => {
+    postCadastro: (req, res) => {
+        let error = validationResult(req)
+        if (error.isEmpty()) {
+            const {
+                nome,
+                sobrenome,
+                email,
+                senha,
+                endereco,
+                data,
+            } = req.body
+            const cryptSenha = bcrypt.hashSync(senha, 10)
+            const user = {
+                nome,
+                sobrenome,
+                email,
+                senha: cryptSenha,
+                admin: false,
+                endereco,
+                data,
+            }
+            Usuario.save(user)
+            return res.redirect('/')
+        }
+        return res.render('home/cadastro', { listaDeErros: error.errors, old: req.body })
+    },
+    login: (req, res) => {
         res.render('home/login')
     },
-    perfil: (req, res, next) => {
+    perfil: (req, res) => {
         res.render('home/perfil')
     },
-    historico: (req, res, next) => {
+    historico: (req, res) => {
         res.render('home/historico')
     },
-    carrinho: (req, res, next) => {
+    carrinho: (req, res) => {
         res.render('home/carrinho')
     },
-    produtos: (req, res, next) => {
+    produtos: (req, res) => {
         res.render('home/produtos')
     }
 
--- routes/home.js
+++ routes/home.js
@@ -4,9 +4,11 @@ const homeController = require('../controllers/homeController')
 
 router.get('/', homeController.index)
 router.get('/cadastro', homeController.cadastro)
+router.post('/cadastro', homeController.postCadastro)
 router.get('/login', homeController.login)
 router.get('/perfil', homeController.perfil)
 router.get('/historico', homeController.historico)
+router.get('/cadastro', homeController.cadastro)
 router.get('/produtos', homeController.produtos)
 
 module.exports = router
