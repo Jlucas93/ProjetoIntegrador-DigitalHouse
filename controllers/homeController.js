2c6017919c50572baaa6a35956cf7767ef5eee0d
jlnascimentoandrade@.gmail.com <you@example.com>
2022-07-06 09:17:26 -0300

    fixing homeController

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,28 +1,28 @@
 const Produto = require("../Models/produto")
 
 const homeController = {
-    index: (req, res) => {
-        const produtos = Produto.findAll()
-        if (req.session.user) {
-            res.render('home/index', { produtos, user: req.session.user })
-        }
-        res.render('home/index', { produtos })
-    },
-    cadastro: (req, res) => {
-        res.render('home/cadastro')
-    },
-    perfil: (req, res) => {
-        res.render('home/perfil')
-    },
-    historico: (req, res) => {
-        res.render('home/historico')
-    },
-    carrinho: (req, res) => {
-        res.render('home/carrinho')
-    },
-    produtos: (req, res) => {
-        res.render('home/produtos')
+  index: (req, res) => {
+    const produtos = Produto.findAll()
+    if (req.session.user) {
+      return res.render('home/index', { produtos, user: req.session.user })
     }
+    res.render('home/index', { produtos })
+  },
+  cadastro: (req, res) => {
+    res.render('home/cadastro')
+  },
+  perfil: (req, res) => {
+    res.render('home/perfil')
+  },
+  historico: (req, res) => {
+    res.render('home/historico')
+  },
+  carrinho: (req, res) => {
+    res.render('home/carrinho')
+  },
+  produtos: (req, res) => {
+    res.render('home/produtos')
+  }
 
 }
 module.exports = homeController
