1d6d7c3c565e605406861da4d79bfa3ddd660b76
João Lucas Nascimento Andrade <jlnascimentoandrade@gmail.com>
2022-08-31 21:28:14 -0300

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,7 +11,7 @@ const homeController = {
       return res.render("home/index", { produtos })
     } catch (error) {
       console.error(error)
-      return res.status(500).JSON({ message: 'Bad Request' })
+      return res.status(500).json({ message: 'Bad Request' })
     }
   },
   cadastro: (req, res) => {
@@ -28,7 +28,7 @@ const homeController = {
       return res.render("home/detalhes", { produto, user: req.session.user })
     } catch (error) {
       console.error(error)
-      return res.status(500).JSON({ message: 'Bad Request' })
+      return res.status(500).json({ message: 'Bad Request' })
     }
   },
   contato: (req, res) => {
